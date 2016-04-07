d866554f02b2ab36466289c6852977c3645e2c42
guokeke <guo.ke.ke.a@gmail.com>
2016-04-07 21:54:08 +0800

    添加api文档

--- src/client/static/index/app.js
+++ src/client/static/index/app.js
@@ -1,19 +1,20 @@
 // created by guokeke @ 16.4.5
-// 这是首页脚本
+// 首页脚本
 //
 $(function(){
   //
-  $.ajax({
-      url:'/api/v1/news/11/',
-      // dataType:'json',
-      contentType:'application/json',
-      method:'get',
-      data:JSON.stringify({news_title:'aaa'})
-  })
-  .error(function(err){
-    console.log(err)
-  })
-  .success(function(res){
-    console.log(res)
-  })
+
+  // $.ajax({
+  //   url:'/api/v1/news/',
+  //   method:'post',
+  //   data:JSON.stringify({news_title:'666'}),
+  //   contentType:'application/json'
+  // })
+  // .done(function(data){
+  //   console.log(data);
+  // })
+  // .fail(function(err){
+  //   console.log(err);
+  // })
+
 })
