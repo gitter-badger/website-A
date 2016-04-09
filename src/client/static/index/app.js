7479ccc42b464f228edc2bb051d269bad4e8064a
guokeke <guo.ke.ke.a@gmail.com>
2016-04-09 17:48:16 +0800

    首页图片预览

--- src/client/static/index/app.js
+++ src/client/static/index/app.js
@@ -3,18 +3,86 @@
 //
 $(function(){
   //
+  var app = function(){
+    return app.prototype.init()
+  }
 
-  // $.ajax({
-  //   url:'/api/v1/news/',
-  //   method:'post',
-  //   data:JSON.stringify({news_title:'666'}),
-  //   contentType:'application/json'
-  // })
-  // .done(function(data){
-  //   console.log(data);
-  // })
-  // .fail(function(err){
-  //   console.log(err);
-  // })
+  function getWindowInnerHeight(){
+    return window.innerHeight
+  }
+
+  function getWindowWidth(){
+    return window.innerWidth
+  }
+
+  function preLoadImage(img,src,callback){
+    $(img).attr('src',src)
+    $(img).load(callback);
+  }
+
+  app.prototype = {
+    init:function(){
+
+      this.headerHeight = getWindowInnerHeight()
+      this.headerWidth = getWindowWidth()
+
+      return this.setup()
+    },
+    setup:function(){
+
+      this.setupHeader()
+
+      return this.bind()
+    },
+    bind:function(){
+
+      return this
+    },
+
+    setupHeader:function(){
+      var self = this,
+          canvas = $('.head-canvas'),
+          ctx = canvas[0].getContext('2d'),
+          image = $('.head-image-image'),
+          cover = $('.head-image-cover')
+
+      this.setWidth(canvas,this.headerWidth)
+      this.setHeight(canvas,this.headerHeight)
+      // 这是预览效果
+      var m = new Image()
+      m.src = 'http://7xsoiv.com2.z0.glb.clouddn.com/img-1.jpg?imageView2/2/w/500/h/500/interlace/0/q/100'
+      m.crossOrigin = "Anonymous";
+      m.onload = function(){
+        ctx.drawImage(m,0,0,self.headerWidth,self.headerHeight)
+        StackBlur.canvasRGBA(canvas[0], 0, 0, self.headerWidth, self.headerHeight, 8)
+      }
+      this.preLoadHeaderImage(image,'//7xsoiv.com2.z0.glb.clouddn.com/img-1.jpg',function(){
+        image.addClass('head-image-image-active')
+        canvas.addClass('head-canvas-hide')
+      })
+      this.setHeight(image,this.headerHeight)
+      return this
+    },
+
+    preLoadHeaderImage:function(img,src,callback){
+      preLoadImage(img,src,callback)
+    },
+
+    setWidth:function(e,width){
+      $(e).attr('width',width)
+    },
+
+    setHeight:function(e,height){
+      $(e).attr('height',height)
+    },
+
+    setupNav:function(){
+
+    }
+
+
+  }
+
+  app();
 
 })
