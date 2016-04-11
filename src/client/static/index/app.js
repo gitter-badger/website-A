c3cd1df67948591c10d8c2e8f3551e4517a211bd
guokeke <guo.ke.ke.a@gmail.com>
2016-04-11 23:11:22 +0800

    制作页面

--- src/client/static/index/app.js
+++ src/client/static/index/app.js
@@ -3,31 +3,17 @@
 //
 $(function(){
   //
-<<<<<<< HEAD
-  $.ajax({
-      url:'/api/v1/news/11/',
-      dataType:'json',
-      contentType:'application/json',
-      method:'get',
-      data:JSON.stringify({news_title:'aaa'})
-  })
-  .error(function(err){
-    console.log(err)
-  })
-  .success(function(res){
-    console.log(res)
-  })
-=======
+
   var app = function(){
     return app.prototype.init()
   }
 
-  function getWindowInnerHeight(){
+  function getHeight(){
     return window.innerHeight
   }
 
-  function getWindowWidth(){
-    return window.innerWidth
+  function getWidth(){
+    return window.screen.width
   }
 
   function preLoadImage(img,src,callback){
@@ -38,8 +24,8 @@ $(function(){
   app.prototype = {
     init:function(){
 
-      this.headerHeight = getWindowInnerHeight()
-      this.headerWidth = getWindowWidth()
+      this.headerHeight = getHeight()
+      this.headerWidth = getWidth()
 
       return this.setup()
     },
@@ -60,22 +46,27 @@ $(function(){
           canvas = $('.head-canvas'),
           ctx = canvas[0].getContext('2d'),
           image = $('.head-image-image'),
-          cover = $('.head-image-cover')
+          cover = $('.head-image-cover'),
+          head = $('.head')
 
-      this.setWidth(canvas,this.headerWidth)
-      this.setHeight(canvas,this.headerHeight)
-      this.setHeight(image,this.headerHeight)
-      
+      this.setWidth(head[0],this.headerWidth)
+      this.setWidth(canvas[0],this.headerWidth)
+      this.setWidth(image[0],this.headerWidth)
+
+      this.setHeight(head[0],this.headerHeight)
+      this.setHeight(canvas[0],this.headerHeight)
+      this.setHeight(image[0],this.headerHeight)
+
       // 这是预览效果
       var m = new Image()
-      m.src = 'http://7xsoiv.com2.z0.glb.clouddn.com/img-1.jpg?imageView2/2/w/500/h/500/interlace/0/q/100'
-      // 图片跨源
-      m.crossOrigin = "Anonymous";
       m.onload = function(){
         ctx.drawImage(m,0,0,self.headerWidth,self.headerHeight)
         // 将图片模糊
         StackBlur.canvasRGBA(canvas[0], 0, 0, self.headerWidth, self.headerHeight, 8)
       }
+      m.crossOrigin = "anonymous"
+      m.src = 'http://7xsoiv.com2.z0.glb.clouddn.com/img-1.jpg?imageView2/2/w/500/h/500/interlace/0/q/100'
+      // 图片跨源
       this.preLoadHeaderImage(image,'//7xsoiv.com2.z0.glb.clouddn.com/img-1.jpg',function(){
         image.addClass('head-image-image-active')
         canvas.addClass('head-canvas-hide')
@@ -105,5 +96,4 @@ $(function(){
 
   app();
 
->>>>>>> 2193feb067e44a70cb325cefc0d8003a8f06371f
 })
